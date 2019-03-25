9251b2f01a7000aa8c550c2905361d6a2226a51e
crane93 <youl9094@gmail.com>
2019-03-25 17:26:35 +0900

    0325

--- webdeign/js/script.js
+++ webdeign/js/script.js
@@ -1,20 +1,34 @@
-$(function(){
-			//$("#nav>ul>li").hover(function(){<!-- 이벤트 핸들러,-->
-				$("#nav>ul>li").hover(function(){
-					$(this).children(".depth2").stop().slideDown(200);
-					$(this).addClass("on");
-			}, function(){
-				$(".depth2").stop().slideUp(200);
-				$(this).removeClass("on");
-			});//거짓은 콤마
+$(function() {
+				
+	//  $("#nav>ul>li").hover(function() {
+	//	  $(this).children(".depth2").slideDown(200);
+	//  }, function() {
+	//	$(".depth2").slideUp(200);
+	//  });
+	$("#nav>ul>li").mouseenter(function() {
+			$(this).children(".depth2").stop().slideDown(200);
+			$(this).addClass("on");
+		})
+	$("#nav>ul>li").mouseleave(function() {
+		$(this).children(".depth2").stop().slideUp(200);
+		$(this).removeClass("on");
+		});
 		
-	
-	$('.bxslider').bxSlider({	//j쿼리 다운받은거임,
-		mode: 'vertical',
+		
+
+	$('.bxslider').bxSlider({
+		mode: 'fade',
 		auto: true,
 		captions: true,
 		speed:500,
-		slideWidth: 0			//화면에 꽉차게 보임
+		slideWidth:0
 	});
+	
+		
+}); //end
 
-});		//index에 있는 스크립트를 따로 js 파일로 만들음
+
+
+
+
+
